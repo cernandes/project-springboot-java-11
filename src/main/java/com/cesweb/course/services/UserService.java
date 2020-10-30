b221222835150eac8fc64e7d5139679512dce327
Claudio <cernandes.silva@gmail.com>
2020-10-30 19:37:40 -0300

    Service layer, component registration

---
+++ src/main/java/com/cesweb/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.cesweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cesweb.course.entities.User;
+import com.cesweb.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
