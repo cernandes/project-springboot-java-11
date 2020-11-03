2af14da7e1510e1f286fa005242db65beb995027
Claudio <cernandes.silva@gmail.com>
2020-11-03 00:04:23 -0300

    Exception handling - delete

---
+++ src/main/java/com/cesweb/course/services/exceptions/DataBaseException.java
@@ -0,0 +1,9 @@
+package com.cesweb.course.services.exceptions;
+
+public class DataBaseException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public DataBaseException(String msg) {
+		super(msg);
+	}
+}
